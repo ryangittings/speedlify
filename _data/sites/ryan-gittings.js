0bfed89d1247517b683e11358104dc7e71390692
Ryan Gittings <ryan@madebykingfisher.co.uk>
2021-05-26 17:06:15 +0100

    Update site config

--- _data/sites/ryan-gittings.js
+++ _data/sites/ryan-gittings.js
@@ -1,16 +1,10 @@
 module.exports = {
-  name: 'ryangittings.co.uk', // optional, falls back to object key
+  name: 'ryangittings.co.uk',
   description: "Ryan's Personal web site",
-  // skip if localhost
-  // skip if this is a new fork of the speedlify (not Zach’s)
-  skip: !process.env.CONTEXT || process.env.SITE_NAME !== 'speedlify',
+  urls: ['https://www.ryangittings.co.uk/', 'https://www.ryangittings.co.uk/services/', 'https://www.ryangittings.co.uk/work/', 'https://www.ryangittings.co.uk/about/', 'https://www.ryangittings.co.uk/contact/'],
+  skip: false,
   options: {
-    frequency: 60 * 23, // 23 hours
-    // Use "run" if the sites don’t share assets on the same origin
-    //           and we can reset chrome with each run instead of
-    //           each site in every run (it’s faster)
-    // Use "site" if sites are all on the same origin and share assets.
+    frequency: 1,
     freshChrome: 'site',
   },
-  urls: ['https://www.ryangittings.co.uk/', 'https://www.ryangittings.co.uk/services/', 'https://www.ryangittings.co.uk/work/', 'https://www.ryangittings.co.uk/about/', 'https://www.ryangittings.co.uk/contact/'],
 };
